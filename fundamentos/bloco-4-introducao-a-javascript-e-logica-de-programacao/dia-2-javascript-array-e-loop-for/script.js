2edb619cb791e279c43c8a569fd2ad2c41a350dc
Edgard <edgardfix@hotmail.com>
2022-04-08 15:41:12 -0300

    adiciona exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,5 +1,31 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// 1 -Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
-for (var i = 0; i < numbers.length; i += 1){
-    console.log(numbers[i]);
+// for (var i = 0; i < numbers.length; i += 1){
+//     console.log(numbers[i]);
+// }
+
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+// for (var i = 0; i < numbers.length; i += 1) {
+//     sum += numbers[i]
+// }
+
+// for (var number of numbers) {
+//     sum += number
+// }
+
+// console.log(sum);
+
+// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let media = 0;
+
+for (var valor of numbers) {
+  sum += valor;
 }
+
+media = sum / numbers.length;
+
+console.log(media);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/test.js
@@ -0,0 +1,11 @@
+let fruits = [3, 4, 1, 1, 1];
+let sum = 0;
+for (var number of fruits) {
+  sum += number;
+}
+
+if (sum >= 15) {
+  console.log(sum);
+} else {
+  console.log("Valor menor que 16");
+}
