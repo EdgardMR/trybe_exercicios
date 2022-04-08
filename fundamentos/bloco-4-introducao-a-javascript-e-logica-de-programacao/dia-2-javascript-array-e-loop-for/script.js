9e5b4c3cb37a82d9c62aa87a02b32712f2d5af2c
Edgard <edgardfix@hotmail.com>
2022-04-08 18:29:44 -0300

    adiciona exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -52,8 +52,25 @@
 
 // Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
+// let higherNumber = 0;
+
+// for (var number of numbers) {
+//     if (number > higherNumber) {
+//         higherNumber = number
+//     }
+// }
+
+// console.log(higherNumber)
+
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
-let media = 0;
 
+let higherNumber = 0;
+
+for (var number of numbers) {
+    if (number > higherNumber) {
+        higherNumber = number
+    }
+}
+
+console.log(higherNumber)
