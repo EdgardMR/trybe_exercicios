4804696f0f8d95bf9149113bba33c27be54a8cea
Edgard <edgardfix@hotmail.com>
2022-04-08 20:55:15 -0300

    adiciona solução do exercicio 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -86,6 +86,19 @@
 // ###################################################################################################
 
 
+// Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+
+// let menorNumero = numbers[0];
+
+// for (var index = 1; index < numbers.length; index++) {
+    
+//     if (numbers[index] < menorNumero) {
+//         menorNumero = numbers[index];
+//     }
+// }
+// console.log(menorNumero);
+
+
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
