9b9c0d85a438b7396211f09e81041a098d1085cb
Edgard <edgardfix@hotmail.com>
2022-04-08 16:02:18 -0300

    adiciona exercício 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -4,6 +4,8 @@
 //     console.log(numbers[i]);
 // }
 
+// ###################################################################################################
+
 // Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
 // for (var i = 0; i < numbers.length; i += 1) {
@@ -15,17 +17,43 @@
 // }
 
 // console.log(sum);
+// ###################################################################################################
 
 // Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+
+// for (var valor of numbers) {
+//   sum += valor;
+// }
+
+// media = sum / numbers.length;
+
+// console.log(media);
+
+// ###################################################################################################
+
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+// for (var valor of numbers) {
+//     sum += valor;
+//   }
+  
+//   media = sum / numbers.length;
+  
+//   console.log(media);
+  
+//   if (media > 20) {
+//       console.log("valor maior que 20")
+//   } else {
+//       console.log("valor menor ou igual a 20")
+//   }
+
+// ###################################################################################################
+
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 let media = 0;
 
-for (var valor of numbers) {
-  sum += valor;
-}
-
-media = sum / numbers.length;
-
-console.log(media);
